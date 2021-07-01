edebcd441f4261f63f1df41096e306774b73d579
Hongxuan Yue <conquistadore@126.com>
2021-07-01 17:52:54 +0800

    Practice on instantiating an object of List as ArrayList

--- src/CollectionPractice/List/ListDemo.java
+++ src/CollectionPractice/List/ListDemo.java
@@ -1,10 +1,28 @@
 package CollectionPractice.List;
 
+import java.util.*;
+
 public class ListDemo {
     public static void main(String[] args) {
         /** list: allow repeated data
          *
          * */
+        //List<TestList> myList = new List<TestList>();
+        /** You cannot instantiate a list as above since List is AN INTERFACE
+         * Either:
+         * 1. use List.of(...)
+         * 2. instantiate any subclass of List:
+         *      eg. List<MYTYPE> = new ArrayList<MYTYPE>
+         * */
+        List<TestList> testList = new ArrayList<>();
+        testList.add(new TestList(1,"First element",114.514));
+        testList.add(new TestList(2,"Second element",19.19));
+        testList.add(new TestList(3,"Third element",8.10));
+        testList.add(new TestList(4,"Forth element",12.450));
 
+        System.out.println(testList);/** println() accept an ArrayList object */
+        testList.forEach(
+                (testList1) -> {System.out.println(testList1);}
+        );/** Or use lambda */
     }
 }
--- src/CollectionPractice/List/TestList.java
+++ src/CollectionPractice/List/TestList.java
@@ -1,7 +1,37 @@
 package CollectionPractice.List;
 
-public class TestList {
+class TestList {
     private int var1;
     private String var2;
     private double var3;
+
+    public TestList(int var1, String var2, double var3) {
+        this.var1 = var1;
+        this.var2 = var2;
+        this.var3 = var3;
+    }
+
+    public int getVar1() {
+        return var1;
+    }
+
+    public void setVar1(int var1) {
+        this.var1 = var1;
+    }
+
+    public String getVar2() {
+        return var2;
+    }
+
+    public void setVar2(String var2) {
+        this.var2 = var2;
+    }
+
+    public double getVar3() {
+        return var3;
+    }
+
+    public void setVar3(double var3) {
+        this.var3 = var3;
+    }
 }
