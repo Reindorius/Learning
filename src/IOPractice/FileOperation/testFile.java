c73dd098b0e855763317af1ced0040057f653657
Reindorius_Desktop_Home <conquistadore@126.com>
2021-07-11 22:17:29 +0800

    today's learning

--- src/IOPractice/FileOperation/testFile.java
+++ src/IOPractice/FileOperation/testFile.java
@@ -12,6 +12,10 @@ public class testFile {
         else System.out.println(file.createNewFile());
          */
 
+        /**
+         * Create a .txt file under the designated directory
+         * If the directory does not exist yet, the directory, under which the .txt file should be created, will be
+         * created first. Here the parent directory is relative to the .txt file.
         File file = new File("F:" + File.separator + "JAVA_CODE" + File.separator + "Learning" + File.separator +
                 "files" + File.separator + "file1.txt");
 
@@ -19,6 +23,49 @@ public class testFile {
 
         if(file.exists()) file.delete();
         else System.out.println(file.createNewFile());
+         */
+
+        /**
+         * obtain all sub-files of the designated directory
+        File file = new File("f:" + File.separator);
+        if(file.isDirectory())
+        {
+            File allFiles[] = file.listFiles();
+            for(int i = 0; i < allFiles.length; i++)
+                System.out.println(allFiles[i]);
+        }
+         */
+
+        /**
+         * Printing all files under the path F:/JAVA_CODE/Learning/src
+        File file = new File("F:" + File.separator + "JAVA_CODE" + File.separator +
+                "Learning" + File.separator + "src");
+        listFile(file);
+         */
+
+
+    }
+
+    public static void listFile(File file)
+    {
+        /**
+         * A method listing all sub-files of the designated file path recursively
+         */
+        if(file.isDirectory())
+        {
+            File subFiles[] = file.listFiles();
+
+            if(subFiles!=null)
+            {
+                for(int i = 0; i < subFiles.length; i++)
+                {
+                    listFile(subFiles[i]);
+                }
+            }
+        }
+        else
+            System.out.println(file);
     }
 }
+
 
---
+++ src/IOPractice/FileStream/testStream.java
@@ -0,0 +1,7 @@
+package IOPractice.FileStream;
+
+public class testStream {
+    public static void main(String[] args) {
+
+    }
+}
